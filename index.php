62f3527463fe974f6ded0e37b2cef2a441d7ce0c
Ted Michael Celis <krazete@gmail.com>
2020-03-01 09:34:07 -0800

    messed up format a little, added test page

--- index.php
+++ index.php
@@ -11,52 +11,53 @@
         <label><input type="radio" name="type" value="char">Character</label>
         <label><input type="radio" name="type" value="armor">Armor</label>
         <label><input type="radio" name="type" value="agon">Agon</label>
-        <p>lol</p>
     </div>
-    <input id="moniker-color-top" class="jscolor {hash:true}" value="#ffca1a">
-    <input id="moniker-color-bottom" class="jscolor {hash:true}" value="#fe6207">
     <canvas id="card-canvas"></canvas>
-    <div id="card">
-        <img id="bg" src="img/bg/large/Background_01.jpg">
-        <img id="moniker-bg" src="img/Nome.png">
-        <input id="moniker" type="text" placeholder="Modifica nome PG" autocomplete="off">
-        <canvas id="moniker-canvas"></canvas>
-        <div id="infobox" class="colonna">
-            <!-- <img id="info" src="img/Colonna.png"> -->
-            <input id="type" type="text" placeholder="Type">
-            <div id="stat">
+    <div>
+        <div id="card" style="float:left">
+            <img id="bg" src="img/bg/large/Background_01.jpg">
+            <img id="moniker-bg" src="img/Nome.png">
+            <input id="moniker" type="text" placeholder="Modifica nome PG" autocomplete="off">
+            <canvas id="moniker-canvas"></canvas>
+            <div id="infobox" class="colonna">
+                <!-- <img id="info" src="img/Colonna.png"> -->
+                <input id="type" type="text" placeholder="Type">
+                <div id="stat">
 <?php foreach (array("ra", "at", "de", "vo", "eq") as $id) { ?>
-                <div id="<?php echo $id; ?>">
+                    <div id="<?php echo $id; ?>">
 <?php for ($i = 0; $i < 6; $i++) { ?>
-                    <label><input type="radio" name="<?php echo $id; ?>"></label>
+                        <label><input type="radio" name="<?php echo $id; ?>"></label>
+<?php } ?>
+                    </div>
 <?php } ?>
                 </div>
-<?php } ?>
-            </div>
-            <div id="armorstat">
+                <div id="armorstat">
 <?php foreach (array("st", "da") as $id) { ?>
-                <div id="<?php echo $id; ?>">
+                    <div id="<?php echo $id; ?>">
 <?php for ($i = 0; $i < 7; $i++) { ?>
-                    <label><input type="radio" name="<?php echo $id; ?>"></label>
+                        <label><input type="radio" name="<?php echo $id; ?>"></label>
+<?php } ?>
+                    </div>
 <?php } ?>
                 </div>
-<?php } ?>
-            </div>
-            <div id="moves">
+                <div id="moves">
 <?php for ($i = 0; $i < 8; $i++) { ?>
-                <div id="move-<?php echo $i; ?>">
-                    <input class="name" type="text" placeholder="Nome tecnica">
-                    <input class="pa" type="text" placeholder="PA">
-                    <input class="bonus" type="text" placeholder="Bonus">
-                </div>
+                    <div id="move-<?php echo $i; ?>">
+                        <input class="name" type="text" placeholder="Nome tecnica">
+                        <input class="pa" type="text" placeholder="PA">
+                        <input class="bonus" type="text" placeholder="Bonus">
+                    </div>
 <?php } ?>
+                </div>
             </div>
         </div>
-    </div>
-    <div>
-        <div class="option-label" name="Name Color"></div>
-        <div class="option-label" name="Background Color"></div>
-        <div class="option-label" name="Image"></div>
+        <div>
+            <div class="option-label" name="Name Color"></div>
+            <input id="moniker-color-top" class="jscolor {hash:true}" value="#ffca1a">
+            <input id="moniker-color-bottom" class="jscolor {hash:true}" value="#fe6207">
+            <div class="option-label" name="Background Color"></div>
+            <div class="option-label" name="Image"></div>
+        </div>
     </div>
 </body>
 </html>
